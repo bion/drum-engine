198338bbc6f980103b25c88272621367e31aef42
bion <bionjohnson@gmail.com>
2016-04-24 22:04:12 -0700

    Start on GUI

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://zombo.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[seesaw "1.4.5"]
+                 [org.clojure/clojure "1.8.0"]
                  [overtone "0.10.1"]])
--- src/drum_engine/core.clj
+++ src/drum_engine/core.clj
@@ -1,15 +1,19 @@
 (ns drum-engine.core
   [:require
-   [overtone.core :as ot]])
+   [overtone.core :as ot]]
+  [:use [seesaw.core]])
+
+(def nugs-num-samples 10)
 
 (if-not (ot/server-connected?) (ot/boot-server))
 
 ;; load drum samples into map with k/v pairs:
 ;; { :sample-name #<buffer[live]: the-sample.aiff> }
 ;;
-;; each sample has keys
+;; each sample has keys:
 ;; :id :size :n-channels :rate :status :path
 ;; :args :name :rate-scale :duration :n-samples
+;; :spectral-group
 (def drum-samples
   (reduce
    (fn [sample-map samp]
@@ -17,9 +21,53 @@
                            :name
                            (clojure.string/split  #"\.")
                            first
-                           keyword)]
-       (if (contains? sample-map sample-name)
-         (throw (Exception. "not cool, duplicate sample name: " sample-name)))
+                           keyword)
+           spectral-group (-> samp
+                              :path
+                              (clojure.string/split #"\/")
+                              reverse
+                              second
+                              keyword)
+           samp (assoc samp :spectral-group spectral-group)]
+
        (assoc sample-map sample-name samp)))
    {}
    (ot/load-samples "resources/samples/**/*")))
+
+(ot/defsynth play-buf-simple [buf 0 amp 1]
+  (let [sig (ot/play-buf:ar :num-channels 1
+                            :bufnum buf
+                            :rate (ot/buf-rate-scale:kr buf)
+                            :action 2)]
+    (ot/out:ar 0 (* amp sig))))
+
+(native!)
+
+(defn combobox-for [spectral-group]
+  (let [samples (filter #(= spectral-group (:spectral-group %)) drum-samples)
+        names (map :name samples)]
+    (combobox :model names)))
+
+(defn sample-select [num]
+  (let [label (str "Pad " num ":")
+        boxes (combobox-for :med)]
+    [label boxes]))
+
+(def gui-content
+  (flow-panel
+   :align :left
+   :hgap 20
+   :items (flatten (map sample-select (range nugs-num-samples)))))
+
+(def seesaw-frame
+  (frame :title "MIDI Sample Manager"
+         :size [800 :by 600]
+         :on-close :exit))
+
+(invoke-later
+ (-> seesaw-frame
+     pack!
+     show!))
+
+(config! seesaw-frame :size [800 :by 600])
+(config! seesaw-frame :content gui-content)
