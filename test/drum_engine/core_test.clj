3c5ec5bd63d47ba2b9df2f9b06b84109fb98dfec
Andrew Jacobson <andrew.isaac.jacobson@gmail.com>
2016-08-29 21:56:33 -0700

    First passing test!

--- test/drum_engine/core_test.clj
+++ test/drum_engine/core_test.clj
@@ -1,3 +1,16 @@
 (ns drum-engine.core-test
-  (:require [clojure.test :refer :all]
-            [drum-engine.core :refer :all]))
+  (:use [clojure.test]
+        [drum-engine.core]
+        [drum-engine.storage]
+        [seesaw.core]))
+
+(defn test-setup []
+  (let [db-path "/tmp/drum-engine-test-db"]
+    (if (db-present? db-path) (clojure.java.io/delete-file db-path))
+    (start db-path)))
+
+(deftest sample-is-selected-in-combobox-on-load-test
+  (test-setup)
+  (let [children (seesaw.util/children (.getContentPane midi-sample-manager-frame))
+        first-sample-label (text (first children))]
+    (is (= "Nugs pad 1:" first-sample-label))))
