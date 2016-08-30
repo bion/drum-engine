cb5c04b63f341f3daf95aed05f6e058976cbf4f1
bion <bionjohnson@gmail.com>
2016-08-29 20:58:32 -0700

    Inject db path at runtime

--- src/drum_engine/core.clj
+++ src/drum_engine/core.clj
@@ -6,10 +6,18 @@
    [drum-engine.play]
    [drum-engine.sample-library]])
 
-(def store
-  (let [config (if (db-present?)
-                 (load-pad-samples)
-                 (do (create-db!) (load-pad-samples)))]
+(declare store)
+(declare db-path)
+(declare render)
+
+(def default-db-name ".midi-sample-manager-storage.db")
+(def default-db-path
+  (str user-home-dir "/" default-db-name))
+
+(defn get-store []
+  (let [config (if (db-present? db-path)
+                 (load-pad-samples db-path)
+                 (do (create-db! db-path) (load-pad-samples db-path)))]
     (atom config)))
 
 (def midi-sample-manager-frame
@@ -17,12 +25,11 @@
          :size [1000 :by 1300]
          :on-close :exit))
 
-(declare render)
 (defn sample-selected [id sample-name]
   (swap! store (fn [state]
                  (update-in state [id]
                             assoc :sample-name (keyword sample-name))))
-  (update-pad-sample! id sample-name)
+  (update-pad-sample! db-path id sample-name)
   (println (get @store id))
   (render))
 
@@ -30,11 +37,18 @@
   (current-sample-set! @store)
   (config! midi-sample-manager-frame :content (gui-content @store sample-selected)))
 
+(defn start
+  ([] (start default-db-path))
+  ([path]
+   (def db-path path)
+   (def store (get-store))
+   (render)))
+
 (native!) ;; lolwut
 
 (defn -main [& args]
   (invoke-later (show! midi-sample-manager-frame)
-                (render)))
+                (start)))
 
 ;; (-main)
 ;; (render)
